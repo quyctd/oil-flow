a92a9b0feef180dea01e518e87d458ed8dbd27e1
quyctd <skyvp98@gmail.com>
2018-06-12 22:31:40 +0700

    hope it run:(

--- src/oilDev/settings/production.py
+++ src/oilDev/settings/production.py
@@ -4,6 +4,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["oilflow.herokuapp.com",'127.0.0.1']
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
 CORS_REPLACE_HTTPS_REFERER = True
 HOST_SCHEME = "https://"
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
